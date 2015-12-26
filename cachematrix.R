800482f19d9030bed1a606e20a694a0e0695772c
vikulp <svikulp@gmail.com>
2015-12-26 12:59:10 +0530

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
-## Write a short comment describing this function
+## Initializing a Z global matrix
+z<-matrix(nrow=3, ncol=3)
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  z<<-x
+  z1<<-solve(x)
+  z1
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above. If the inverse has already 
+## been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+check<-identical(x,z)
+check1<-as.numeric(check)
+   if(check1==1)
+  {
+    inv<-z1
+    print(x)
+  }
+  else
+  {
+    inv<-makeCacheMatrix(x)
+    print(a)
+  }
+  inv
 }
+
